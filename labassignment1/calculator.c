35ddebf1d018158e766cc40c07ec868c4c75b5dd
Katie Davenport <kdavenpo@login-students.khoury.northeastern.edu>
2023-01-18 20:46:30 -0500

    upload version 1 - assignment 1

--- labassignment1/calculator.c
+++ labassignment1/calculator.c
@@ -1,10 +1,13 @@
-//enter your name here
-//enter your email here
+//Katie Davenport 
+//davenport.k@northeastern.edu
 #include<stdio.h>
+
 int main()
 {
     int num1,num2,answer,choice;
-    
+ 
+    //1 = addition, 2 = subtraction, 3 = mult ... 
+   
     printf("Enter your choice\n");
     printf(" 1.addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n");
     scanf("%d",&choice);
@@ -12,7 +15,28 @@ int main()
     printf("Enter num1 and num2 values");
     scanf("%d %d", &num1, &num2);
     
-    
-           
+    switch (choice) {
+    case 1:
+	answer = num1 + num2;
+	printf("Addition\nSum= %d\n", answer);
+    case 2:
+	answer = num1 - num2;
+	printf("Subtraction\nDifference = %d\n", answer); 	
+    case 3:
+	answer = num1 * num2;
+	printf("Multiplication\nProduct = %d\n", answer);
+    case 4:
+	if(num2 == 0) {
+	printf("*** You cannot divide by zero ***\n");
+	} else { 
+	answer = num1 / num2;      // Check no remainder 
+	int remainder = num1 % num2; // Check modulo 
+	printf("Division\nQuotient = %d\n", answer);
+	printf("Remainder = %d:", remainder);
+	}
+    default:
+	printf("Invalid input\n");
+	break;
+  } 
     return 0;
 }
