ee8f882f4974c66aa750e9cc968687e623ef6690
Katie Davenport <kdavenpo@login-students.khoury.northeastern.edu>
2023-01-20 17:32:24 -0500

    upload employee program draft

--- labassignment1/employee.c
+++ labassignment1/employee.c
@@ -1,6 +1,10 @@
+//Katie Davenport
+//davenport.k@northeastern.edu
+
 /*C Program to read and print the n employee details using structure and dynamic memory allocation*/
 #include <stdio.h>
 #include <stdlib.h>
+
 /*structure to hold the details of an employee*/
 typedef struct employee
 {
@@ -10,24 +14,45 @@ typedef struct employee
     char Dept[10];
 
 }employee_t;
+
 /* Read the employee details using scanf*/
 void readData(int n, employee_t* s)
 {
-    
+  int i;
+  for(i=0; i<n, n++){
+    printf("Enter empID, Name, Designation, and Dept: \n");   
+    scanf("%d %s %s %s", d->empID, s->Name, s->Designation, s->Dept);
 }
+
 /* Function to print the employee details*/
 void display(int n, employee_t * s)
 {
-    
-    
+     
 }
 
 /*----------Main Program--------*/
 int main()
 
 {
+int n;// n is the number of employees 
+scanf("%d",&n);// to take the user input 
 
-    
+// dynamic memory allocation - in the heap 
+employee_t * s = (employee_t*)malloc(n * sizeof(employee_t)); 
+
+// will return generic pointer to heep   
+
+// null pointer means no memory allocated 
+  if(s!=NULL) {
+    readData(n,s);
+    display(n,s);
+    free(s);
+  }
+}
+  
+  }
+  RETURN 0;
+}    
 
 }
 
