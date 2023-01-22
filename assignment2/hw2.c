5c4bd0d78ad3ca97af40b2e3ce85206480aa456e
kndavenpo <122413804+kndavenpo@users.noreply.github.com>
2023-01-22 13:36:12 -0500

    Update hw2.c

--- assignment2/hw2.c
+++ assignment2/hw2.c
@@ -1,43 +1,66 @@
-// name: <your name here>
-// email: <your email here>
-
+// name: Katie Davenport
+// email: davenport.k@northeastern.edu
 #include <stdio.h>   // stardard input/output library
 #include <stdbool.h> // standard boolean library: bool, true, false
 
 #define MAXSIZE 100
 
+int stack[MAXSIZE];
+
+// s is an int array, t is top index
 bool isEmpty (int* s, int t) {
+  // check that the array is not null
   // returns true if t = -1
-
-  // INSERT YOUR CODE HERE
+  if (t < 0) {
+  return true;
+  } else {
+  return false;
+  }
 }
 
+// s is an int array, t is top index
 bool isFull (int* s, int t) {
+  // check that the array is not null
   // returns true if no more room in the stack
-
-  // INSERT YOUR CODE HERE
+  if (t >= MAXSIZE-1) {
+  return true;
+  } else {
+  return false;
+  }
 }
 
+// Update top(t) so it is passed by reference
+// S is an array so inside the push function you will access with s[i]
+// tp is a pointer to int so access it with *tp
 void push(int v, int* s, int* tp) {
   // put v onto the top of the stack s unless it is already full
-
-  // INSERT YOUR CODE HERE
+  if (!isFull(s,*tp)) {
+                (*tp)++;
+                s[*tp] = v;}
+  else {printf("*** attempt to push %d onto full stack ***\n", v);}
 }
 
+// Update top(t) so it is passed by reference
 int pop (int* s, int* tp) {
   // return the top entry in the stack unless stack is empty
   // update s and *tp -- requires top to be passed by reference!
-
-  // INSERT YOUR CODE HERE
+  int returnval = 0;
+  if (!isEmpty(s, *tp)) {
+        returnval = s[*tp];
+        (*tp)--;
+  } else {
+        printf("*** attempt to pop empty stack ***\n");
+  }
+  return returnval;
 }
 
 int main () {
-
+  //2 stack arrays and 2 top ints
   int stack1[MAXSIZE]; // array in which stack will live
   int top1 = -1;       // top valid location in stack, -1 == empty
   int stack2[MAXSIZE]; // array in which stack will live
   int top2 = -1;       // top valid location in stack, -1 == empty
-  
+
   printf("pushing: 1, 2, 3, 4, 5 onto first stack\n");
   printf("pushing: 100, 200, 300, 400, 500 onto second stack\n\n");
   push(1, stack1, &top1);
