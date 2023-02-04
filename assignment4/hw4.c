a355cf196709be63efb28b7cd5b03fa95cd78862
kndavenpo <kdavenpo@login-students.khoury.northeastern.edu>
2023-02-04 10:53:37 -0500

    submit homework 4

--- assignment4/hw4.c
+++ assignment4/hw4.c
@@ -1,32 +1,21 @@
-// name: <your name here>
-// email: <your email here>
+// name: Katie Davenport 
+// email: davenport.k@northeastern.edu
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
 
-#define LIMIT 50
+#define LIMIT 50  
 #define RAND_RANGE 100
 
 int main(){
-
   int source[LIMIT]; // array to hold input data values
-  int dest[LIMIT];   // array to hold sorted data values
-                     // use dest only if you are using two arrays
-  bool valid[LIMIT]; // array that indicates which input values are valid
-
   int i;             // loop variable
   int j;             // loop variable
-  int smallest;      // current smallest element
 
   //seed random numbers
   srand((unsigned)time(NULL));
-
-  //initialize valid array - at begining the full array is valid
-  for (i=0; i<LIMIT; i++) {
-    valid[i] = true;
-  }
 
   //initialize source array with random numbers from 0..RAND_RANGE
   for (i=0; i<LIMIT; i++) {
@@ -45,21 +34,32 @@ int main(){
   }
   printf("\n");
 
-  //selection sort
-  for (i=0; i<LIMIT; i++) {
-
-    // INSERT YOUR CODE HERE
-    
+  // helper function to swap
+  void swap(int* a, int* b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
   }
-  
-  //print out sorted array in rows of 10
-  printf("Destination array:\n");
-  
 
-  // INSERT YOUR CODE HERE
-    
- 
-  
-  
+  //selection sort 
+  for (i=0; i<LIMIT; i++) {
+	for(j=i+1; j<LIMIT; j++) {
+		if (source[j] <= source[i]){
+		swap(&source[j], &source[i]);	
+		}		
+	}  	
+  }
+   
+ //print out sorted array in rows of 10
+ printf("Destination array:\n");
+    for (i=0; i < ((LIMIT/10)+1); i++) {
+    for (j=0; j<10; j++) {
+      if (i*10+j < LIMIT) {
+        printf("%.2d ",source[i*10+j]);
+      }
+    }
+    printf("\n");
+  }
+  printf("\n");
   return 0;
 }
