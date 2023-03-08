9e22e20086d8c79a552c0e4ffa57309adfaa34bb
kndavenpo <kdavenpo@login-students.khoury.northeastern.edu>
2023-03-08 16:11:29 -0500

    submit lab7

--- labassignment7/lab7.c
+++ labassignment7/lab7.c
@@ -1,6 +1,5 @@
-
-/*Enter your name here*/
-/* Enter your email here*/
+/* Katie Davenport */
+/* davenport.k@northeastern.edu*/
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -18,15 +17,16 @@ typedef struct node
 /* Inserting patients in the queue based on priority*/
 node_t *insert(struct node *front)
 {
-
     node_t *p,*temp;
-    char name[20],address[100];
+    //char name[20],address[100];
     temp=(node_t*)malloc(sizeof(node_t));
     printf("Enter patient Name:");
+    //fgets(temp->name, strlen(temp->name), stdin); // Use to allow for white spaces
     scanf("%s", temp->name);
     printf("Enter the patient's age:");
     scanf("%d",&(temp->age));
     printf("Enter the address:");
+    //fgets(temp->address, strlen(temp->address), stdin); // Use to allow for white spaces
     scanf("%s",temp->address);
     printf("Enter the reg no:");
     scanf("%d",&(temp->reg));
@@ -34,17 +34,56 @@ node_t *insert(struct node *front)
     scanf("%d",&(temp->priority));
     int pri=temp->priority;
 
- /*insert your code here*/
-
+ // Case: queue is empty or new priority is highest
+ // Add temp to the front
+ if(front == NULL || pri > front->priority){
+	temp->next = front; 
+	front = temp;
+	return front; 
+} 
+ // Else: Loop through the queue so long as the priority of the next node is greater than 
+ // the priority of the temp and we have not reached the end of the queue
+ 
+  p = front; 
+  while(p->next != NULL && p->next->priority >= pri){
+	p = p->next;
+	}
+ 	
+	temp->next = p->next;
+ 	p->next = temp; 
+ 	return front; 
 
 }
+ 
+/* Function to free the nodes*/
+void freenode(node_t *temp)
+{
+     if(temp!=NULL){
+     freenode(temp->next);
+     free(temp);
+     }
+}
+
 
 /* Delete the node which is present at the front*/
 node_t *delete(struct node *front)
 {
-    
-    /*Insert your code here*/
-            
+node_t *temp = front; 
+
+if(front == NULL) {
+	return front;
+} if (front->next == NULL){
+	free(front);
+	front = NULL;  
+	return front; 
+} else { 	
+	temp = front->next;
+	front->next = NULL;
+	free(front);
+	front = temp;
+	return front;            
+}
+
 }
 
 /* To display the patients records */
@@ -67,14 +106,8 @@ void display(node_t *front)
     }
     return;
 }
-/* Function to free the nodes*/
-void freenode(node_t *temp)
-{
-    if(temp!=NULL){
-    freenode(temp->next);
-    free(temp);
-    }
-}
+
+
 /*-----------------Main program---------------------------*/
 int main()
 {
