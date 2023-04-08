8e6c7fadd0fc4312117987b3321907e50e643771
kndavenpo <kdavenpo@login-students.khoury.northeastern.edu>
2023-04-08 13:56:07 -0400

    hw12 submission

--- assignment12/hw12.c
+++ assignment12/hw12.c
@@ -1,5 +1,5 @@
-// name: <your name here>
-// email: <your email here>
+// name: Katie Davenport
+// email: davenport.k@northeastern.edu
 // Compile with:
 //
 // gcc -lpthread hw12.c -o hw12
@@ -16,6 +16,7 @@
 // shared variable
 int counter = 0;
 
+
 // thread to be executed - unspecified variable arguments
 void* thread1 (void* vargp) {
   // add 1 to counter
@@ -26,16 +27,24 @@ void* thread1 (void* vargp) {
 void* thread2 (void* vargp) {
   // add 5 to counter
   // *** YOUR CODE GOES HERE ***
+ 
+  counter = counter +5;
+  return NULL;
 }
 
 void* thread3 (void* vargp) {
   // subtract 2 from counter
   // *** YOUR CODE GOES HERE ***
+  counter = counter -2;
+  return NULL;
 }
 
 void* thread4 (void* vargp) {
   // subtract 10 from counter
   // *** YOUR CODE GOES HERE ***
+ 
+  counter = counter -10;
+  return NULL; 
 }
 
 int main() {
@@ -53,6 +62,14 @@ int main() {
     // now create the 2nd, 3rd, 4th group of 100 threads
     
     // *** YOUR CODE GOES HERE ***
+    // create the second 100 threads using thread2 code
+    pthread_create(&(tid[100 + i]), NULL, thread2, NULL);
+
+    // create the third 100 threads using thread3 code
+    pthread_create(&(tid[200 + i]), NULL, thread3, NULL);
+
+    // create the fourth 100 threads using thread4 code 
+    pthread_create(&(tid[300 + i]), NULL, thread4, NULL);
   }
 
   //wait until ALL 400 threads are done
